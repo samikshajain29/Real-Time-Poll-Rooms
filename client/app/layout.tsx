fcb91eade69fc3ebe778e54f29a7fb70eb910261
Samiksha Jain <samikshaj7371@gmail.com>
2026-02-16 20:59:44 +0530

    UI improvement

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,15 +2,15 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import { WebSocketProvider } from "@/context/WebSocketContext"; // <-- Import the provider
+import { WebSocketProvider } from "@/context/WebSocketContext";
 
 // Configure the font
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '600', '700', '800'] 
+  subsets: ["latin"],
+  weight: ["400", "600", "700", "800"],
 });
 export const metadata: Metadata = {
-  title: "Live Poll Battle",
+  title: "Real-Time Poll Rooms",
   description: "A real-time polling application",
 };
 
@@ -22,9 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={poppins.className}>
-        <WebSocketProvider> {/* <-- Wrap children with the provider */}
-          {children}
-        </WebSocketProvider>
+        <WebSocketProvider>{children}</WebSocketProvider>
       </body>
     </html>
   );
